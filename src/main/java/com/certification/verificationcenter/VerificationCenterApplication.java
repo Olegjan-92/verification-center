4e79411e06271c90c7b16932e71f47cf681804ba
Пономарев Олег <ponomarev.oa@reliab.tech>
2020-03-16 19:00:27 +0300

    initial commit

---
+++ src/main/java/com/certification/verificationcenter/VerificationCenterApplication.java
@@ -0,0 +1,13 @@
+package com.certification.verificationcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificationCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerificationCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/certification/verificationcenter/VerificationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certification.verificationcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificationCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
